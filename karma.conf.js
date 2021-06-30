caf2679ef11ab2d8cbae115f2c2ec7a517a8862d
Alan Acuña <alantheace@outlook.com>
2021-06-30 18:50:27 -0500

    Changed project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/demo-ar'),
+      dir: require('path').join(__dirname, './coverage/demo-angular-ar'),
       subdir: '.',
       reporters: [
         { type: 'html' },
