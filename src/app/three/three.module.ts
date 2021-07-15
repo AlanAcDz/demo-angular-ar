7636edcb992bda5c078793bed582f8261528b0e9
Alan Acuña <alantheace@outlook.com>
2021-07-15 18:50:19 -0500

    Removed unsused code

--- src/app/three/three.module.ts
+++ src/app/three/three.module.ts
@@ -1,34 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ThreeCoreModule, LoaderService } from '@angular-three/core';
-import { ThreeMeshModule } from '@angular-three/core/meshes';
-import { ThreeBoxBufferGeometryModule } from '@angular-three/core/geometries';
-import { ThreeMeshBasicMaterialModule } from '@angular-three/core/materials';
-
 import { ModelLoaderService } from './model-loader.service';
-
 import { ThreeSceneComponent } from './three-scene/three-scene.component';
-import { AngularThreeComponent } from './angular-three/angular-three.component';
 
 @NgModule({
   declarations: [
     ThreeSceneComponent,
-    AngularThreeComponent,
   ],
   exports: [
     ThreeSceneComponent,
-    AngularThreeComponent,
   ],
   imports: [
     CommonModule,
-    ThreeCoreModule,
-    ThreeMeshModule,
-    ThreeBoxBufferGeometryModule,
-    ThreeMeshBasicMaterialModule,
   ],
   providers: [
-    LoaderService,
     ModelLoaderService,
   ],
 })
